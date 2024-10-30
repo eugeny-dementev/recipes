53b39f72e38b4cc1d504f3fdd76e0738ec313256
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-30 22:45:18 +0800

    copy assert lib

---
+++ assert.js
@@ -0,0 +1,65 @@
+export const assert = {
+  is(value, reference, message) {
+    if (value === reference) {
+      return true;
+    }
+
+    throw new TypeError(message);
+  },
+
+  isString(value, message) {
+    if (typeof value === 'string') {
+      return;
+    }
+
+    throw new TypeError(message);
+  },
+
+  isBoolean(value, message) {
+    if (typeof value === 'boolean') {
+      return;
+    }
+
+    throw new TypeError(message);
+  },
+
+  isObject(value, message) {
+    if (typeof value === 'object' && !Array.isArray(value) && value !== null) {
+      return;
+    }
+
+    throw new TypeError(message);
+  },
+
+  isFunction(value, message) {
+    if (typeof value === 'function') {
+      return;
+    }
+
+    throw new TypeError(message);
+  },
+
+  oneOf(value, values, message) {
+    if (values.includes(value)) {
+      return
+    }
+
+    throw new TypeError(message || `value should be on of ${values.join(',')} but it is: ${value}`);
+  },
+
+  isInstanceOf(value, klass, message) {
+    if (value instanceof klass) {
+      return;
+    }
+
+    throw new TypeError(message);
+  },
+
+  isFalse(value, message) {
+    if (value === false) {
+      return
+    }
+
+    throw new TypeError(message);
+  },
+};
