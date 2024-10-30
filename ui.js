99a2e28649ca4d0c8f738c3ca454c6d51c40dd4f
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-30 23:24:02 +0800

    add renderer for recipes

--- ui.js
+++ ui.js
@@ -1,4 +1,6 @@
+import { assert } from "./assert.js";
 import { Ingredients } from "./ingredients.js";
+import { recipes } from "./recipes.js";
 import { state } from "./state.js";
 
 export function renderIngredientTags() {
@@ -17,6 +19,8 @@ export function renderIngredientTags() {
       }
 
       console.log('Выбранные ингредиенты:', state.selectedIngredients);
+
+      renderRecipes();
     });
 
     tagsContainer.appendChild(button)
@@ -37,17 +41,20 @@ export function renderModeSelector() {
   anyButton.innerHTML = 'Любой из выбранных';
   allButton.innerHTML = 'Каждый из выбранных';
 
-
   anyButton.id = 'any_mode';
   allButton.id = 'all_mode';
 
   function switchMode(event) {
     const button = event.target;
 
+    state.searchMode = button.id;
+
     anyButton.classList.remove('selected');
     allButton.classList.remove('selected');
 
     button.classList.add('selected');
+
+    renderRecipes();
   }
 
   anyButton.addEventListener('click', switchMode);
@@ -57,3 +64,68 @@ export function renderModeSelector() {
   modeContainer.appendChild(allButton);
 }
 
+export function renderRecipes() {
+  const recipesContainer = document.getElementById('recipes');
+  recipesContainer.innerHTML = '';
+
+  let counter = 0; // render no more than 3 recipes on the page
+
+  for (let i = 0; i < recipes.length && counter < 3; i++) {
+    const recipe = recipes[i];
+
+    const renderCurrentRecipe = showRecipe(state.searchMode, recipe.ingredients);
+
+    console.log({ renderCurrentRecipe, size: state.selectedIngredients.size, ingredients: recipe.ingredients.join() });
+
+    if (!renderCurrentRecipe) {
+      continue;
+    }
+
+    const recipeContainer = document.createElement('div');
+    recipeContainer.classList.add('recipe_container');
+
+    const titleEl = document.createElement('div');
+    titleEl.classList.add('recipe_title');
+    titleEl.innerHTML = recipe.name;
+    recipeContainer.appendChild(titleEl);
+
+    const descriptionEl = document.createElement('div');
+    descriptionEl.classList.add('recipe_description');
+    descriptionEl.innerHTML = recipe.name;
+    recipeContainer.appendChild(descriptionEl);
+
+    recipesContainer.appendChild(recipeContainer);
+
+    counter++; // count each rendered recipe
+  }
+}
+
+function showRecipe(mode, ingredients) {
+  assert.oneOf(mode, ['any_mode', 'all_mode'], 'Есть только два режима поиска');
+
+  if (state.selectedIngredients.size === 0) {
+    return true;
+  }
+
+  if (mode === 'any_mode') {
+    for (const ingredient of ingredients) {
+      if (state.selectedIngredients.has(ingredient)) {
+        return true
+      }
+    }
+
+    return false;
+  }
+
+  if (mode === 'all_mode') {
+    for (const ingredient of state.selectedIngredients.values()) {
+      if (!ingredients.includes(ingredient)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  return false;
+}
