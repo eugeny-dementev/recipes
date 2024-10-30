043f95f0b307377a5eedca8afd77e6034ca3a0df
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-30 20:33:11 +0800

    extract js into separate file

---
+++ index.js
@@ -0,0 +1,42 @@
+
+const Ingredients = {
+  Potato: 'картошка',
+  Carrot: 'морковь',
+  Tomato: 'томат',
+};
+
+const Recipes = [
+  {
+    name: 'Рецепт номер 1',
+    ingrediehts: [
+      Ingredients.Carrot,
+      Ingredients.Potato,
+    ],
+    description: 'Какой то описание 1',
+    steps: [
+      'Шаг 1: что-то',
+      'Шаг 2: что-то',
+    ],
+  },
+  {
+    name: 'Рецепт номер 2',
+    ingrediehts: [
+      Ingredients.Tomato,
+      Ingredients.Potato,
+    ],
+    description: 'Какой то описание 2',
+    steps: [
+      'Шаг 1: что-то',
+      'Шаг 2: что-то',
+      'Шаг 3: что-то',
+    ],
+  },
+];
+
+
+function renderer(recipes) {
+
+}
+
+function validator(recipe) { }
+
