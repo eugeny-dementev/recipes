708cae6fe42cb4974f687d6e629ebb12daccf0b2
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-10-30 22:55:04 +0800

    cleanup index.js

--- index.js
+++ index.js
@@ -1,40 +1,4 @@
-import { Ingredients } from "./ingredients.js";
-import { Recipe } from "./recipe.js";
 import { renderIngredientTags, renderModeSelector } from "./ui.js";
-
-
-const recipes = [
-  new Recipe('Рецепт номер 1')
-    .addDescription('Описание рецепта 1')
-    .addIngredient(Ingredients.Potato)
-    .addStep({
-      position: 1,
-      description: 'Порезать картошку',
-    })
-    .get(),
-  {
-    name: 'Рецепт номер 2',
-    ingrediehts: [
-      Ingredients.Tomato,
-      Ingredients.Potato,
-    ],
-    description: 'Какой то описание 2',
-    steps: [
-      'Шаг 1: что-то',
-      'Шаг 2: что-то',
-      'Шаг 3: что-то',
-    ],
-  },
-];
-
-console.log('Site with recipes', recipes)
 
 renderModeSelector();
 renderIngredientTags();
-
-function renderer(recipes) {
-
-}
-
-function validator(recipe) { }
-
