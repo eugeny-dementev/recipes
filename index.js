2c67184044f3197b6c60cbf3f874be721b051bb8
Eugeny Dementyev <eugeny.dementev@ya.ru>
2024-11-01 20:42:52 +0800

    connect route file

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+import { getRoute } from "./router.js";
 import { renderIngredientTags, renderModeSelector, renderRecipes } from "./ui.js";
+
+const pageContext = getRoute();
+
+console.log('route:', pageContext);
 
 renderModeSelector();
 renderIngredientTags();
